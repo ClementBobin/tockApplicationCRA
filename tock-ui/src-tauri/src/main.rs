fba45e90066d58bb525e192fa16a21fada771bb4
mirage <119869686+ClementBobin@users.noreply.github.com>
2026-01-12 11:13:15 +0100

    feat: Simplify main function by removing Tauri builder and using tock_ui_lib

--- tock-ui/src-tauri/src/main.rs
+++ tock-ui/src-tauri/src/main.rs
@@ -2,8 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    tock_ui_lib::run()
 }
